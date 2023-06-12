37d917dc8a56bea7c49d891dd797ecd9e2c39a8f
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-12 16:47:09 -0400

    Update Two_sum.java

--- Two_sum.java
+++ Two_sum.java
@@ -8,13 +8,13 @@ You can return the answer in any order.
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n=nums.length;
-        Map<Integer,Integer> map=new HashMap<>();
-        int[] result=new int[2];
-        for(int i=0;i<n;i++){
+        int n = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for(int i = 0; i < n; i++){
             if(map.containsKey(target-nums[i])){
-                result[1]=i;
-                result[0]=map.get(target-nums[i]);
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
                 return result;
             }
             map.put(nums[i],i);
